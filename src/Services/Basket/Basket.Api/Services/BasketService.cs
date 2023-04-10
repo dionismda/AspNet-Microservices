f72d07cf0eadb09fc786cc31a72814c32e24677a
dionismda <dionismda@gmail.com>
2023-04-10 16:52:49 -0300

    Integrate basket api with discount.grpc

---
+++ src/Services/Basket/Basket.Api/Services/BasketService.cs
@@ -0,0 +1,24 @@
+﻿namespace Basket.Api.Services;
+
+public class BasketService : IBasketService
+{
+    private readonly IBasketRepository _repository;
+    private readonly DiscountGrpcService _discountGrpcService;
+
+    public BasketService(IBasketRepository repository, DiscountGrpcService discountGrpcService)
+    {
+        _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        _discountGrpcService = discountGrpcService;
+    }
+
+    public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+    {
+        foreach (var item in basket.Items)
+        {
+            var coupon = await _discountGrpcService.GetDiscount(item.ProductName);
+            item.Price -= coupon.Amount;
+        }
+
+        return await _repository.UpdateBasket(basket);
+    }
+}
