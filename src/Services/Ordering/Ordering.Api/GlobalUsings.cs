43b23ffce953ce697bc9e334262de294838e884b
dionismda <dionismda@gmail.com>
2023-04-11 13:13:56 -0300

    Create Ordering microservice and implemented domain and application layer

---
+++ src/Services/Ordering/Ordering.Api/GlobalUsings.cs
@@ -0,0 +1,3 @@
+global using Microsoft.OpenApi.Models;
+global using Ordering.Api;
+global using Ordering.Application;
---
+++ src/Services/Ordering/Ordering.Application/Mappers/OrderMapper.cs
@@ -0,0 +1,11 @@
+﻿namespace Ordering.Application.Mappers;
+
+public class OrderMapper : Profile
+{
+    public OrderMapper()
+    {
+        CreateMap<Order, OrderViewModel>().ReverseMap();
+        CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
+        CreateMap<Order, UpdateOrderCommand>().ReverseMap();
+    }
+}
